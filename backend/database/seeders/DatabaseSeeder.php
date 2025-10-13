4ef911d66d09b9ef15fca59e55867a3abee56be0
hudectomas <121263060+hudectomas@users.noreply.github.com>
2025-10-13 15:00:20 +0200

    Update DatabaseSeeder.php

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
+        // Vytvorí testovacieho používateľa
+        User::create([
             'email' => 'test@example.com',
+            'role' => 'student',
+            'pwd' => Hash::make('password'),
         ]);
     }
 }
