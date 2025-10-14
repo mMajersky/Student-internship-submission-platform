5ad44fb00584d165718fa07a7bfac44527491b25
Hitori <fekecfilip@gmail.com>
2025-10-14 19:43:42 +0200

    delete register path in router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
 import DashboardView from '../views/DashboardView.vue'
 const LandingView = () => import('../views/LandingView.vue')
 
@@ -17,11 +16,7 @@ const router = createRouter({
       name: 'login',
       component: LoginView
     },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView
-    },
+
     {
       path: '/dashboard',
       name: 'dashboard',
