cfcd4c930ce7875c1783457f689972f989689434
Hitori <fekecfilip@gmail.com>
2025-11-17 15:50:42 +0100

    Add Cache facade import to GarantController
    Imported Illuminate\Support\Facades\Cache in GarantController.php to enable caching functionality within the controller.

--- backend/app/Http/Controllers/GarantController.php
+++ backend/app/Http/Controllers/GarantController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
+
 
 class GarantController extends Controller
 {
